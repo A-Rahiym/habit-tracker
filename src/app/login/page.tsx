8c180b939e072e2f3c7764b6c9966f95fa5d62f6
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-04-27 14:36:21 +0100

    refactor: loginform moved to component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,67 +1,7 @@
 "use client";
-import { useState, useEffect } from "react";
-import { User } from "@/src/types/auth";
-import { useRouter } from "next/navigation";
-import { getUsers, saveSession } from "@/src/lib/storage";
+
+import { LoginForm } from "@/src/components/auth/LoginForm";
 
 export default function LoginPage() {
-
-    const router = useRouter();
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-
-    const handleLogin = () => {
-        const users = getUsers()
-        const user = users.find(
-            u => u.email === email && u.password === password
-        );
-
-        if (!user) {
-            alert('Invalid email or password');
-        }
-
-        saveSession({
-            userId: user.Id,
-            email: user.email
-        })
-
-        router.push('/dashboard')
-    }
-
-
-    return (
-        <div className="h-screen flex items-center justify-center px-6">
-            <div className="w-full max-w-sm space-y-4">
-                <h1 className="text-xl font-semibold">
-                    Login
-                </h1>
-
-                <input
-                    className="w-full border rounded-xl p-3 focus:outline-none focus:ring-2"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <input
-                    className="w-full border rounded-xl p-3 focus:outline-none focus:ring-2"
-                    placeholder="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button
-                    onClick={handleLogin}
-                    className="w-full bg-violet-600 text-white py-3 rounded-xl active:scale-[0.99]"
-                >
-                    Login
-
-                </button>
-                <p className="text-sm text-center text-white cursor-pointer"
-                    onClick={() => router.push('/signup')}
-                >
-                    Create Account
-                </p>
-            </div>
-        </div>
-    )
+  return <LoginForm />;
 }
