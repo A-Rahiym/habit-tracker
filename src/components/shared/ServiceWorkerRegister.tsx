6ecab367ac0a3873b126420f9c92a96be2ee6f91
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-04-29 12:36:13 +0100

    fix: added check for app to see if its in production or development begore worker is initialised

--- src/components/shared/ServiceWorkerRegister.tsx
+++ src/components/shared/ServiceWorkerRegister.tsx
@@ -1,10 +1,11 @@
-// src/components/shared/ServiceWorkerRegister.tsx
 'use client';
 
 import { useEffect } from 'react';
 
 export function ServiceWorkerRegister() {
   useEffect(() => {
+    if (process.env.NODE_ENV !== 'production') return;
+
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
         .register('/sw.js')
